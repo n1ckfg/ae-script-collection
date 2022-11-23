7b74f7b0b7b4e5eaabb25d5a80328a1c94398d55
Nick Fox-Gieg <nick@fox-gieg.com>
2022-11-22 19:27:50 -0500

    first

---
+++ ae-template/ae-template.jsx
@@ -0,0 +1,94 @@
+{
+		
+// Sample Panel Setup
+
+function buildUI(this_obj_) {
+    var win = (this_obj_ instanceof Panel) ? this_obj_ : new Window('palette', 'Script Window',[760,345,1120,597]);
+
+    //Jeff Almasol's solution to fix text color
+    var winGfx = win.graphics;
+    var darkColorBrush = winGfx.newPen(winGfx.BrushType.SOLID_COLOR, [0,0,0], 1);
+
+    //-----------------------------------------------------
+    //1. Draw buttons
+    //buttons coordinates are X start, Y start, X end, Y end
+    var butYoffset = 10; //8;
+    var butYoffsetCap = 4;
+    //--
+    var butXstart = 8;
+    var butXend = 149;//152;
+    var butYstart = 15 + butYoffset;
+    var butYend = 43 + butYoffset;
+    var butYinc = 30;
+    //--
+    var colXstart = 4;
+    var colXend = 165;
+    var colYstart = 4 + butYoffset; //4;
+    var colYendBase = 33;
+    var colXinc = 170;
+
+    //Basic button group
+    var col1butCount = 1;
+    win.panel1Group = win.add('panel', [colXstart+(colXinc * 0),colYstart,colXend+(colXinc*0),colYendBase+(col1butCount*butYinc)+butYoffset+butYoffsetCap], "", {borderStyle: "etched"});
+    win.panel1Group0 = win.panel1Group.add('button', [butXstart,butYstart+(butYinc*0),butXend,butYend+(butYinc*0)], 'Nulls for Pins');
+
+    //--
+    //Character button group
+    var col2butCount = 0;
+    //win.panel2Group = win.add('panel', [colXstart+(colXinc * 1),colYstart,colXend+(colXinc*1),colYendBase+(col2butCount*butYinc)], 'Rigging', {borderStyle: "etched"});
+    win.panel2Group = win.add('panel', [colXstart+(colXinc * 0),colYstart,colXend+(colXinc*0),colYendBase+(col2butCount*butYinc)+butYoffset+butYoffsetCap], "", {borderStyle: "etched"});
+
+    //--
+    //Advanced button group
+    var col3butCount = 0;
+    //win.panel3Group = win.add('panel', [colXstart+(colXinc * 2),colYstart,colXend+(colXinc*2),colYendBase+(col3butCount*butYinc)], 'Advanced', {borderStyle: "etched"});
+    win.panel3Group = win.add('panel', [colXstart+(colXinc * 0),colYstart,colXend+(colXinc*0),colYendBase+(col3butCount*butYinc)+butYoffset+butYoffsetCap], "", {borderStyle: "etched"});
+
+    //-----------------------------------------------------
+    //2. Link buttons to functions
+    win.panel1Group0.onClick = helloWorld;
+
+    //Tooltips
+    //win.panel1Group0.helpTip = "Creates a controller null for each puppet pin on a layer."; //helloWorld;
+
+    //-----------------------------------------------------
+
+    var selector = win.add("dropdownlist",[colXstart, colYstart, colXend, colYendBase],["Panel1","Panel2","Panel3"]);
+    selector.onChange = function() {
+        if (selector.selection == 0){
+            win.panel1Group.visible = true;
+            win.panel2Group.visible = false;
+            win.panel3Group.visible = false;
+        }else if (selector.selection == 1){
+            win.panel1Group.visible = false;
+            win.panel2Group.visible = true;
+            win.panel3Group.visible = false;
+        }else if (selector.selection == 2){
+            win.panel1Group.visible = false;
+            win.panel2Group.visible = false;
+            win.panel3Group.visible = true;
+        }        
+    }
+    selector.selection = 0;
+
+    return win
+}
+
+var w = buildUI(this);
+
+if (w.toString() == "[object Panel]") {
+    w;
+} else {
+    w.show();
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+function helloWorld(){
+    alert(“Hello world!”);
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+}
---
+++ rd_scripts/rd_scripts_readme.txt
@@ -0,0 +1,33 @@
+rd: scripts
+Copyright (c) 2005-2017 redefinery (Jeffrey R. Almasol). All rights reserved.
+check it: www.redefinery.com
+
+Name: rd_scripts
+Version: 20170108
+
+Description:
+--  ffa: free for all
+    All of my scripts on this site are free for you to use. Unfortunately, 
+    I have less time these days to develop and support them, so they are 
+    provided as-in and without support. However, please respect my copyright.
+
+--  aio: all in one
+    I've provided the latest versions of these scripts in a single download
+    for ease of distribution. The archive will be updated as needed. Please
+    do not redistribute the archive or as separate scripts without my
+    permission.
+
+--  fac: for a cause
+    In lieu of thanks, consider donating to a charity important to you. If
+    my scripts save you time, maybe pass the savings onto organizations
+    doing good work. I'd love to hear how you're paying it forward.
+    #devforacause
+
+
+Legal stuff:
+This bundle of scripts is provided "as is," without warranty of any kind, 
+expressed or implied. In no event shall the author be held liable for any
+damages arising in any way from the use of these scripts.
+
+In other words, I'm just trying to share knowledge with and help out my
+fellow AE script heads, so don't blame me if my code doesn't rate. :-)
